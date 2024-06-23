ebcad776c713fba85aee10b570cafd7d7acd2179
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-23 22:32:10 +0500

    klsdhf

--- resources/views/Admin/category.blade.php
+++ resources/views/Admin/category.blade.php
@@ -1,7 +1,7 @@
 @extends('Admin/layout')
 @section('heading')
 <h1>Category</h1>
-<a href=""><button type="button" class="btn btn-success">Manage Category</button></a>
+<a href="manage_category"><button type="button" class="btn btn-success">Manage Category</button></a>
 <div class="row m-t-30">
     <div class="col-md-12">
         <!-- DATA TABLE-->
