15f171575e3591d927fd28892cbf091b4c5a023b
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-23 20:09:54 +0500

    add section

--- resources/views/Admin/category.blade.php
+++ resources/views/Admin/category.blade.php
@@ -1,1 +1,4 @@
 @extends('Admin/layout')
+@section('header')
+
+@endsection
--- resources/views/Admin/layout.blade.php
+++ resources/views/Admin/layout.blade.php
@@ -245,7 +245,8 @@
                 </div>
             </header>
             <!-- HEADER DESKTOP-->
-
+@section('heading')
+@show
             <!-- MAIN CONTENT-->
             <div class="main-content">
                 <div class="section__content section__content--p30">
