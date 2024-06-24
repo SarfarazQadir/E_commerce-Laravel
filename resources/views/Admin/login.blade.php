439d8c318d5a3f2a377725f20d8c8f684abf6a42
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-24 06:07:43 +0500

    jb

--- resources/views/Admin/login.blade.php
+++ resources/views/Admin/login.blade.php
@@ -43,7 +43,8 @@
                     <div class="login-content">
                         <div class="login-logo">
                             <a href="#">
-                                <img src="{{asset('admin_assets/images/icon/logo.png')}}" alt="CoolAdmin">
+                                {{-- <img src="{{asset('admin_assets/images/icon/logo.png')}}" alt="CoolAdmin"> --}}
+                                {{Config::get('constant.site_name')}}
                             </a>
                         </div>
                         <div class="login-form">
