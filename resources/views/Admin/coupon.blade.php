eda4d1c45c4204597a3253e1666097fe99c96acc
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-27 06:28:14 +0500

    jasbcfla

---
+++ resources/views/Admin/coupon.blade.php
@@ -0,0 +1,44 @@
+@extends('Admin/layout')
+@section('title','Category')
+@section('heading')
+<div class="alert alert-success" role="alert">
+    {{session('message')}}
+</div>
+<h1>Category</h1>
+<br>
+<a href="category/manage_category"><button type="button" class="btn btn-success">Manage Category</button></a>
+<br><br>
+<div class="row m-t-30">
+    <div class="col-md-12">
+        <!-- DATA TABLE-->
+        <div class="table-responsive m-b-40">
+            <table class="table table-borderless table-data3">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>CATEGORY NAME</th>
+                        <th>CATEGORY SLUG</th>
+                        <th>ACTION</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($data as $row)
+                        
+                    
+                    <tr>
+                        <td>{{$row->id}}</td>
+                        <td>{{$row->category_name}}</td>
+                        <td>{{$row->category_slug}}</td>
+                        <td><a href="{{url('admin/category/delete')}}/{{$row->id}}"><button type="button" class="btn btn-danger">Delete</button></a>
+                            <a href="{{url('admin/category/manage_category')}}/{{$row->id}}"><button type="button" class="btn btn-success">Edit</button></a></td>
+                    </tr>
+                    @endforeach
+                    
+                </tbody>
+            </table>
+        </div>
+        <!-- END DATA TABLE-->
+    </div>
+</div>
+@endsection
---
+++ resources/views/Admin/manage_coupon.blade.php
@@ -0,0 +1,42 @@
+@extends('Admin/layout')
+@section('title','Manage Category')
+@section('heading')
+<h1>Manage Category</h1><br>
+<a href="{{url('admin/category')}}"><button type="button" class="btn btn-success">Back</button></a>
+<br><br>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+        <div class="card-body">
+                <form action="{{route('category.manage_category_process')}}" method="post" >
+                    @csrf
+                    <div class="form-group">
+                        <label for="category_name" class="control-label mb-1">Category Name</label>
+                        <input id="category_name" value="{{$category_name}}" name="category_name" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
+                    @error('category_name')
+                    <div class="alert alert-danger" role="alert">
+                        {{$message}}
+                    </div>
+                    @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="category_slug" class="control-label mb-1">Category Slug</label>
+                        <input id="category_slug" name="category_slug" value="{{$category_slug}}" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
+                        @error('category_slug')
+                        <div class="alert alert-danger" role="alert">
+                            {{$message}}
+                        </div>
+                        @enderror
+                    </div>
+                    </div>
+                    <div>
+                        <button id="payment-button" type="submit" class="btn btn-lg btn-info btn-block">
+                           Submit
+                        </button>
+                    </div>
+                    <input type="hidden" value="{{$id}}" name="id" id="id">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
