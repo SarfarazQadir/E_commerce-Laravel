1ac8f70e994905d786de3059c91ec8d257121493
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-27 09:36:27 +0500

    sakjbad

--- resources/views/Admin/layout.blade.php
+++ resources/views/Admin/layout.blade.php
@@ -68,86 +68,13 @@
                                 <a href="{{url('admin/category')}}">
                                      <i class="fas fa-chart-bar"></i>Cetegory</a>
                             </li>
-                                <li>
-                                    <a href="index3.html">Dashboard 3</a>
-                                </li>
-                                <li>
-                                    <a href="index4.html">Dashboard 4</a>
-                                </li>
-                            </ul>
-                        </li>
+                               <li>
                         <li>
-                            <a href="chart.html">
-                                <i class="fas fa-chart-bar"></i>Charts</a>
+                            <a href="{{url('admin/coupon')}}">
+                                 <i class="fas fa-chart-bar"></i>Coupon</a>
                         </li>
-                        <li>
-                            <a href="table.html">
-                                <i class="fas fa-table"></i>Tables</a>
+                       
                         </li>
-                        <li>
-                            <a href="form.html">
-                                <i class="far fa-check-square"></i>Forms</a>
-                        </li>
-                        <li>
-                            <a href="calendar.html">
-                                <i class="fas fa-calendar-alt"></i>Calendar</a>
-                        </li>
-                        <li>
-                            <a href="map.html">
-                                <i class="fas fa-map-marker-alt"></i>Maps</a>
-                        </li>
-                        <li class="has-sub">
-                            <a class="js-arrow" href="#">
-                                <i class="fas fa-copy"></i>Pages</a>
-                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                                <li>
-                                    <a href="login.html">Login</a>
-                                </li>
-                                <li>
-                                    <a href="register.html">Register</a>
-                                </li>
-                                <li>
-                                    <a href="forget-pass.html">Forget Password</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="has-sub">
-                            <a class="js-arrow" href="#">
-                                <i class="fas fa-desktop"></i>UI Elements</a>
-                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                                <li>
-                                    <a href="button.html">Button</a>
-                                </li>
-                                <li>
-                                    <a href="badge.html">Badges</a>
-                                </li>
-                                <li>
-                                    <a href="tab.html">Tabs</a>
-                                </li>
-                                <li>
-                                    <a href="card.html">Cards</a>
-                                </li>
-                                <li>
-                                    <a href="alert.html">Alerts</a>
-                                </li>
-                                <li>
-                                    <a href="progress-bar.html">Progress Bars</a>
-                                </li>
-                                <li>
-                                    <a href="modal.html">Modals</a>
-                                </li>
-                                <li>
-                                    <a href="switch.html">Switchs</a>
-                                </li>
-                                <li>
-                                    <a href="grid.html">Grids</a>
-                                </li>
-                                <li>
-                                    <a href="fontawesome.html">Fontawesome Icon</a>
-                                </li>
-                                <li>
-                                    <a href="typo.html">Typography</a>
-                                </li>
                             </ul>
                         </li>
                     </ul>
@@ -174,6 +101,10 @@
                         <li>
                             <a href="{{url('admin/category')}}">
                                  <i class="fas fa-chart-bar"></i>Cetegory</a>
+                        </li>
+                        <li>
+                            <a href="{{url('admin/coupon')}}">
+                                 <i class="fas fa-chart-bar"></i>Coupon</a>
                         </li>
                        
                         </li>
--- resources/views/Admin/manage_coupon.blade.php
+++ resources/views/Admin/manage_coupon.blade.php
@@ -1,28 +1,28 @@
 @extends('Admin/layout')
-@section('title','Manage Category')
+@section('title','Manage Coupon')
 @section('heading')
-<h1>Manage Category</h1><br>
-<a href="{{url('admin/category')}}"><button type="button" class="btn btn-success">Back</button></a>
+<h1>Manage Coupon</h1><br>
+<a href="{{url('admin/coupon')}}"><button type="button" class="btn btn-success">Back</button></a>
 <br><br>
 <div class="row">
     <div class="col-lg-12">
         <div class="card">
         <div class="card-body">
-                <form action="{{route('category.manage_category_process')}}" method="post" >
+                <form action="{{route('coupon.manage_coupon_process')}}" method="post" >
                     @csrf
                     <div class="form-group">
-                        <label for="category_name" class="control-label mb-1">Category Name</label>
-                        <input id="category_name" value="{{$category_name}}" name="category_name" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
-                    @error('category_name')
+                        <label for="title" class="control-label mb-1">Title</label>
+                        <input id="title" value="{{$title}}" name="title" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
+                    @error('title')
                     <div class="alert alert-danger" role="alert">
                         {{$message}}
                     </div>
                     @enderror
                     </div>
                     <div class="form-group">
-                        <label for="category_slug" class="control-label mb-1">Category Slug</label>
-                        <input id="category_slug" name="category_slug" value="{{$category_slug}}" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
-                        @error('category_slug')
+                        <label for="code" class="control-label mb-1">Code</label>
+                        <input id="code" name="code" value="{{$code}}" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
+                        @error('code')
                         <div class="alert alert-danger" role="alert">
                             {{$message}}
                         </div>
