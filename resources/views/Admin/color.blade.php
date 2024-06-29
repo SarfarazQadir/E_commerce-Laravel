6d1dc78afcbb7f5efc4b6876e1c24e202e740fdf
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-29 09:48:18 +0500

    SBASK

--- resources/views/Admin/color.blade.php
+++ resources/views/Admin/color.blade.php
@@ -1,13 +1,13 @@
 @extends('Admin/layout')
-@section('title','Size')
-@section('size_select','active')
+@section('title','Color')
+@section('color_select','active')
 @section('heading')
 <div class="alert alert-success" role="alert">
     {{session('message')}}
 </div>
 <h1>Category</h1>
 <br>
-<a href="{{url('admin/size/manage_size')}}"><button type="button" class="btn btn-success">Manage size</button></a>
+<a href="{{url('admin/color/manage_color')}}"><button type="button" class="btn btn-success">Manage Color</button></a>
 <br><br>
 <div class="row m-t-30">
     <div class="col-md-12">
@@ -17,6 +17,7 @@
                 <thead>
                     <tr>
                         <th>ID</th>
+                        <th>COLOR</th>
                         <th>SIZE</th>
                         <th>ACTION</th>
                         
@@ -28,15 +29,16 @@
                     
                     <tr>
                         <td>{{$row->id}}</td>
+                        <td>{{$row->color}}</td>
                         <td>{{$row->size}}</td>
                         <td>
-                            <a href="{{url('admin/size/manage_size')}}/{{$row->id}}"><button type="button" class="btn btn-success">Edit</button></a>
+                            <a href="{{url('admin/color/manage_color')}}/{{$row->id}}"><button type="button" class="btn btn-success">Edit</button></a>
                            @if ($row->status == 1)    
-                           <a href="{{url('admin/size/status/0')}}/{{$row->id}}"><button type="button" class="btn btn-primary">Active</button></a>
+                           <a href="{{url('admin/color/status/0')}}/{{$row->id}}"><button type="button" class="btn btn-primary">Active</button></a>
                            @elseif ($row->status == 0)    
-                           <a href="{{url('admin/size/status/1')}}/{{$row->id}}"><button type="button" class="btn btn-warning">Deactive</button></a>  
+                           <a href="{{url('admin/color/status/1')}}/{{$row->id}}"><button type="button" class="btn btn-warning">Deactive</button></a>  
                            @endif
-                            <a href="{{url('admin/size/delete')}}/{{$row->id}}"><button type="button" class="btn btn-danger">Delete</button></a>
+                            <a href="{{url('admin/color/delete')}}/{{$row->id}}"><button type="button" class="btn btn-danger">Delete</button></a>
                         </td>
                     </tr>
                     @endforeach
