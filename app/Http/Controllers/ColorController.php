a41886e31f1e3fc1cb3a7e8d19cfe69f2fad5912
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-29 09:39:49 +0500

    kasjbdak

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -12,7 +12,8 @@ class ColorController extends Controller
      */
     public function index()
     {
-        //
+        $result['data']= Color::all(); 
+        return view('Admin.Color',$result);
     }
 
     /**
@@ -26,15 +27,62 @@ class ColorController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function manage_Color(Request $request,$id ='')
     {
-        //
+        if($id>0){
+            $arr=Color::where(['id' => $id])->get();
+            
+            $result['color']=$arr['0']->size;
+            $result['status']=$arr['0']->status;
+            $result['id']=$arr['0']->id;
+            }else{
+                $result['color']='';
+                $result['status']='';
+                $result['id']='0';
+            }
+        return view('Admin.manage_color',$result);
+    
+}
+    public function manage_color_process(Request $request)
+    {
+        $request->validate([
+            'color'=>'required|unique:colors,color,'.$request->post('id')
+        ]);
+        if($request->post('id')>0){
+            $size = Color::find($request->post('id'));
+            $msg = "Color Updated";
+        }else{
+        $size = new Color();
+        $msg = "Color inserted";
+        }
+        $size->size = $request->post('color');
+        $size->status = 1;
+        // $category->status = 1;
+        $size->save();
+        $request->session()->flash('message',$msg);
+        return redirect('admin/size');
     }
+
+    public function delete(Request $request, $id){
+        $model = Size::find($id);
+        $model->delete();
+        $request->session()->flash('message','Size Deleted');
+        return redirect('admin/size');
+    }
+    public function status(Request $request,$status, $id){
+        $model = Size::find($id);
+        $model->status=$status;
+        $model->save();
+        $request->session()->flash('message','Size Status Updated');
+        return redirect('admin/size');
+    }
+
+    
 
     /**
      * Display the specified resource.
      */
-    public function show(Color $color)
+    public function show(Size $size)
     {
         //
     }
@@ -42,23 +90,32 @@ class ColorController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Color $color)
+    public function edit(string $id)
     {
-        //
+      
+        $size = Size::find($id);
+        return view('Admin.edit_size', compact("size"));
     }
+    
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Color $color)
+    public function update(Request $request, string $id)
     {
-        //
+        $size = Size::find($id);
+        $size->size = $request->size;
+        $size->category_slug = $request->categoryslug;
+        $size->save();
+        $request->session()->flash('message','Size Updated');
+        return redirect('admin/size');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Color $color)
+    public function destroy(Size $size)
     {
         //
     }
