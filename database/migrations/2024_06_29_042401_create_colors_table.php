b4018122f3d3c37adee05b113873a54179e88187
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-29 09:25:08 +0500

    msadnb a

--- database/migrations/2024_06_29_042401_create_colors_table.php
+++ database/migrations/2024_06_29_042401_create_colors_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('colors', function (Blueprint $table) {
             $table->id();
+            $table->string('color');
+            $table->integer('size');
             $table->timestamps();
         });
     }
